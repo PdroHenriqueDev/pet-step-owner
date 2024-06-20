6923a0448fc998ab79e18ced9ea239746015ea76
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-06-20 13:26:33 -0300

    refactor: removing dark mode feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Routes from './routes';
 import {LocationProvider} from './contexts/LocationContext';
 import './styles/global.css';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <LocationProvider>
       <Routes />

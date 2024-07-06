7c466e963a3b665645db7eb17a2409b534c9971b
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-07-06 20:11:02 -0300

    feat: marker

---
+++ src/components/marker/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Icon} from '@rneui/base';
+import colors from '../../styles/colors';
+import {View} from 'react-native';
+import styles from './styles';
+
+function Marker() {
+  return (
+    <View style={styles.markerFixed}>
+      <Icon
+        style={styles.marker}
+        type="font-awesome-6"
+        name="location-pin"
+        size={40}
+        color={colors.dark}
+      />
+    </View>
+  );
+}
+
+export default Marker;
---
+++ src/components/marker/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  markerFixed: {
+    left: '50%',
+    marginLeft: -24,
+    marginTop: -48,
+    position: 'absolute',
+    top: '50%',
+  },
+  marker: {
+    height: 48,
+    width: 48,
+  },
+});
+
+export default styles;

0dfb8e08ba93db63eec6ca22a6c2e69eea1ee88a
PdroHenriqueDev <pedrohenriquedev28@gmail.com>
2024-11-11 20:41:59 -0300

    feat: info privacy

--- src/components/header/notificationHeader.tsx
+++ src/components/header/notificationHeader.tsx
@@ -10,11 +10,11 @@ function NotificationHeader({navigation, route, options}: StackHeaderProps) {
     <View>
       <View style={styles.container}>
         <View style={styles.iconContainer}>
-          {notification && (
+          {/* {notification && (
             <View style={styles.badge}>
               <Text style={styles.badgeText}>1</Text>
             </View>
-          )}
+          )} */}
 
           <Icon type="material" name="notifications-none" size={22} />
         </View>
